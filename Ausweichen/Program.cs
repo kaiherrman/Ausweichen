a847f3c955fec314d4b1395fd58d7e6eb284975e
Kai Herrmann <kai.herrmann@stiecon.de>
2018-03-21 20:43:06 +0100

    Add VS Project

---
+++ Ausweichen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ausweichen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

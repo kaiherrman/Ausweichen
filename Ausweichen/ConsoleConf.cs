013cbeda2c7553ccbedcd3ff6562ac7d0dbfecd8
Kai Herrmann <kai.herrmann@stiecon.de>
2018-03-25 21:26:26 +0200

    Minor Console Conf

---
+++ Ausweichen/ConsoleConf.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ausweichen
+{
+    class ConsoleConf
+    {
+        const int MF_BYCOMMAND = 0x00000000;
+        const int SC_MINIMIZE = 0xF020;
+        const int SC_MAXIMIZE = 0xF030;
+        const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        public void ConsoleSize()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.SetWindowSize(120, 25);
+            Console.SetBufferSize(120, 25);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+        }
+
+        public void Reset()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
--- Ausweichen/Program.cs
+++ Ausweichen/Program.cs
@@ -4,9 +4,30 @@ namespace Ausweichen
 {
     class Program
     {
+        public char[,] grid { get; set; }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //x is left & right
+            //y is up & down
+
+            ConsoleConf confConsole = new ConsoleConf();
+            Program program = new Program();
+
+            confConsole.ConsoleSize();
+            confConsole.Reset();
+            Console.Clear();
+
+            program.grid = new char[61,60]; //First width then length
+
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("   ");
+            confConsole.Reset();
+
+            Console.ReadLine();
         }
     }
 }

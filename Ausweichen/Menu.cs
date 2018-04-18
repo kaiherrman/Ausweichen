31488bed9f2bdffb9e68ed7eadf67ef2b25addd4
Kai Herrmann <kai.herrmann@stiecon,de>
2018-04-18 08:44:17 +0200

    Zwischenpunkt

---
+++ Ausweichen/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ausweichen
+{
+    class Menu
+    {
+        public void PrintMenu()
+        {
+            //Loop um Abstand von oben zu bekommen.
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(System.Environment.NewLine);
+            }
+
+            //string bestehend aus tabs um abstand von links zu bekommen.
+            string centerTabs = "\t\t\t\t\t";
+
+            Console.Write(centerTabs);
+            Console.WriteLine("Highscore: {0}", Highscore()); //TODO: Get highscore from file? 
+            Console.Write(centerTabs);
+            Console.WriteLine("Drücken sie \"Eingabe\" zum starten!");
+            Console.Write(Environment.NewLine);
+            Console.Write(centerTabs);
+            Console.WriteLine("Drücken sie \"H\" für Hilfe");
+        }
+
+        public void Help()
+        {
+            
+        }
+
+        public int Highscore()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Ausweichen\\Highscore.txt";
+            if (File.Exists(path)) //Existiert die Datei?
+            {
+                if(File.ReadAllText(@path).Length != 0) //Befindet sich etwas in der Datei?
+                {
+                    return Convert.ToInt32(File.ReadAllText(@path));
+                }
+            }
+            return 0;
+        }
+    }
+}
--- Ausweichen/Program.cs
+++ Ausweichen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Timer = System.Timers.Timer;
+
 
 namespace Ausweichen
 {
@@ -9,31 +11,50 @@ namespace Ausweichen
         public static int playerPosY { get; set; }
         public static int oldPlayerPosX { get; set; }
         public static int oldPlayerPosY { get; set; }
+        public static int amount { get; set; }
 
         static void Main(string[] args)
         {
+            Menu menu = new Menu();
+            Program program = new Program();
+            menu.PrintMenu();
+
+            //Switch um abzufragen welche Taste gedrückt wird
+            var inputKey = Console.ReadKey().Key;
+            switch (inputKey)
+            {
+                case ConsoleKey.Enter:
+                    program.Play();
+                    break;
+                case ConsoleKey.H:
+                    menu.Help();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Play()
+        {
             //x is left & right
             //y is up & down
-
+            InitTimer();
             ConsoleConf confConsole = new ConsoleConf();
             Program program = new Program();
             confConsole.Reset();
-            //confConsole.ConsoleSize();
-            confConsole.Reset();
-
-            program.grid = new char[29,100]; //First width then height
+            program.grid = new char[29, 100]; 
 
             while (true)
             {
                 Console.Clear();
                 program.grid[oldPlayerPosX, oldPlayerPosY] = ' ';
                 program.grid[playerPosX, playerPosY] = '>';
-
+                GenerateRandomRange(amount);
                 for (int x = 0; x < program.grid.GetLength(0); x++)
                 {
                     for (int y = 0; y < program.grid.GetLength(1); y++)
                     {
-                        if(program.grid[x, y] == '>')
+                        if (program.grid[x, y] == '>')
                         {
                             Console.BackgroundColor = ConsoleColor.Magenta;
                             Console.Write(program.grid[x, y]);
@@ -52,7 +73,7 @@ namespace Ausweichen
                 {
                     case ConsoleKey.UpArrow:
                         oldPlayerPosX = playerPosX;
-                        if(playerPosX != 0)
+                        if (playerPosX != 0)
                         {
                             playerPosX -= 1;
                         }
@@ -68,6 +89,30 @@ namespace Ausweichen
                         break;
                 }
             }
+        }
+
+        Random rand = new Random();
+        public void GenerateRandomRange(int amount)
+        {
+            int[] randNumbers = new int[amount];
+            for(int i = 0; i < amount; i++)
+            {
+                randNumbers[i] = rand.Next(0, 29);
+            }
+        }
+
+        public void InitTimer()
+        {
+            Timer t = new Timer(15000);
+            t.AutoReset = true;
+            t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+            t.Start();
+        }
+
+        private static void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (amount < 28)
+                amount += 1;
         }
 
     }
